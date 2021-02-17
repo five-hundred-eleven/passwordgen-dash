2943e96cf7fc4eb40cd93a99c02574dec6efda48
Erik <ecowley@protonmail.com>
2021-02-17 18:20:24 -0500

    first working prototype

---
+++ PasswordGeneration.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import random
+
+left_keys =         "qwertasdfgzxcv2345"
+left_shift_keys =   "QWERTASDFGZXCV!@#$%"
+
+right_keys =        "yuip[]hjk;'bnm,./6789-="
+right_shift_keys =  'YUIP{}HJKL:"BNM<>?^&*()_+'
+
+all_keys = left_keys + left_shift_keys + right_keys + right_shift_keys
+all_left_keys = left_keys + left_keys + left_shift_keys
+all_right_keys = right_keys + right_keys + right_shift_keys
+
+left_pinky_keys = set(right_shift_keys + "5tgvqaz!")
+right_pinky_keys = set(left_shift_keys + "67yuhbn^&YUHBN-=p[];'/_+P{}:\"<>?")
+all_pinky_keys = left_pinky_keys | right_pinky_keys
+
+left_keys_no_pinky = "".join(set(all_left_keys) - all_pinky_keys)
+right_keys_no_pinky = "".join(set(all_right_keys) - all_pinky_keys)
+
+def generatePassword(num_letters):
+
+    res = ""
+    last_letter = random.choice(all_keys)
+    for _ in range(num_letters):
+
+        if last_letter in all_left_keys:
+
+            if last_letter in all_pinky_keys:
+                letter = random.choice(right_keys_no_pinky)
+            else:
+                letter = random.choice(all_right_keys)
+
+        if last_letter in all_right_keys:
+
+            if last_letter in all_pinky_keys:
+                letter = random.choice(left_keys_no_pinky)
+            else:
+                letter = random.choice(all_left_keys)
+
+        res += letter
+        last_letter = letter
+
+    return res
+
+if __name__ == "__main__":
+
+    print(generatePassword(15))
---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, render_template, request
+from dash import Dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
+from PasswordGeneration import generatePassword 
+
+DEFAULT_PASSWORD_LENGTH = 18
+
+
+external_stylesheets = [
+    dbc.themes.BOOTSTRAP,
+]
+
+meta_tags = [
+    {
+        "name": "viewport",
+        "content": "width=device-width, initial-scale=1",
+    }
+]
+
+FLASK_APP = Flask(__name__)
+APP = Dash(server=FLASK_APP, meta_tags=meta_tags, external_stylesheets=external_stylesheets)
+
+layout = html.Div([
+    dbc.Row([
+        dbc.Col(
+            [],
+            sm=0,
+            md=2,
+            className="buffer",
+        ),
+        dbc.Col(
+            [
+                dcc.Markdown("""
+                    # Generate a Password
+
+                    Generate an easy-to-type and readable random password.
+
+                    Practice entering the password in the input box below the generated password.
+                    
+                    See [the source](https://github.com/ekoly/passwordgen-dash).
+                """),
+                html.Div([], id="length-indicator"),
+                dcc.RangeSlider(
+                    id="password-length",
+                    min=5,
+                    max=80,
+                    step=1,
+                    value=[DEFAULT_PASSWORD_LENGTH,],
+                ),
+                html.Div([], id="generated-password"),
+                dcc.Input(id="confirm-password", style={"width": "100%"}),
+                html.Div([], id="confirm-password-indicator"),
+            ],
+            sm=12,
+            md=6,
+            className="main-area",
+        ),
+        dbc.Col(
+            [],
+            sm=0,
+            md=4,
+            className="buffer",
+        ),
+
+    ])
+])
+
+APP.layout = layout
+
+
+current_generated_password = ""
+
+@APP.callback(
+    [
+        Output("generated-password", "children"),
+        Output("length-indicator", "children"),
+    ],
+    [
+        Input("password-length", "value"),
+    ],
+)
+def updateGeneratedPassword(num_letters):
+    """
+        Update the generated password.
+    """
+
+    global current_generated_password
+
+    num_letters, = num_letters
+
+    print(f"Number of letters selected: {num_letters}")
+
+    indicator_text = f"""
+        Select length with the slider: {num_letters}
+    """
+    current_generated_password = generatePassword(int(num_letters))
+    return [
+        dcc.Markdown(current_generated_password),
+        dcc.Markdown(indicator_text),
+    ]
+
+@APP.callback(
+    Output("confirm-password-indicator", "children"),
+    [
+        Input("confirm-password", "value"),
+    ]
+)
+def confirmPassword(entered_password):
+    """
+        User practices entering the password.
+    """
+
+    global current_generated_password
+
+    if entered_password == current_generated_password:
+        return dcc.Markdown("Correct!", style={"color": "green"})
+    else:
+        return dcc.Markdown("Wrong!", style={"color": "red"})
+
+
+if __name__ == "__main__":
+    APP.run_server(debug=True)
